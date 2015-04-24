dfbb667d217f99668117ce014565a1754a331056
unknown <rsandoval@scoba.me>
2015-04-24 01:18:48 -0500

    Separate job plugins

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersionAttribute("4.2.0.1")]
-[assembly: AssemblyFileVersionAttribute("4.2.0.1")]
+[assembly: AssemblyVersionAttribute("4.2.0.2")]
+[assembly: AssemblyFileVersionAttribute("4.2.0.2")]
